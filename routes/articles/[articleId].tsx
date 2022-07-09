7883d07173d8293facf4d28f1ede66d59b101af1
daiiz <da.iz.mg@gmail.com>
2022-07-10 01:19:23 +0900

    分かってきた

--- routes/articles/[articleId].tsx
+++ routes/articles/[articleId].tsx
@@ -7,11 +7,17 @@ import Article from "../../islands/Article.tsx";
 export default function Greet(props: PageProps) {
   const { articleId } = props.params;
   return (
-    <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <Article
-        title="My fresh だいず"
-        contentHtml="1億年ぶりにブログを作っていくよ。"
-      />
+    <div>
+      <header></header>
+      <main class={tw`p-4 mx-auto max-w-screen-md`}>
+        <Article
+          title="My fresh だいず"
+          contentHtml="1億年ぶりにブログを作っていくよ。"
+        />
+      </main>
+      <footer class={tw`flex justify-center text-xs text-gray-500 my-8`}>
+        <p>Powerd by foo</p>
+      </footer>
     </div>
   );
 }
