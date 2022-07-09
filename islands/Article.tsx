995dd0106904985a6c18cd20cd8a5de23a767f28
daiiz <da.iz.mg@gmail.com>
2022-07-10 02:23:22 +0900

    authorsを下に移動

--- islands/Article.tsx
+++ islands/Article.tsx
@@ -14,7 +14,7 @@ const articleStyle = [
   "article figure { margin: 24px 0; }",
   "article figure .imgs, figcaption { display: flex; justify-content: center; gap: 12px; }",
   "article figure figcaption { color: rgb(55 65 81); margin-top: 4px; }",
-  "article img { max-height: 400px; max-width: 100%; border-radius: 2px; min-width: 0; }", // TODO: 縦長・横長で個別対応
+  "article .imgs img { max-height: 400px; max-width: 100%; border-radius: 2px; min-width: 0; }", // TODO: 縦長・横長で個別対応
 ].join("");
 
 export default function Article(props: ArticleProps) {
@@ -24,18 +24,6 @@ export default function Article(props: ArticleProps) {
       <style dangerouslySetInnerHTML={{ __html: articleStyle }} />
       <article class={tw`w-full font-sans`}>
         <h1 class={tw`text-2xl my-8 mt-10`}>{props.title}</h1>
-        <div class={tw`text-gray-500 mb-4`}>
-          <table class={tw`flex justify-end text-sm`}>
-            <tr>
-              <td>
-                <a href="../updates/2022/07/09">2022-07-09 23:00</a>
-              </td>
-              <td class={tw`pl-4`}>
-                <a href="../authors/だいず">だいず</a>
-              </td>
-            </tr>
-          </table>
-        </div>
         {props.paragraphs.map((paragraph) => {
           return (
             <section
@@ -45,6 +33,36 @@ export default function Article(props: ArticleProps) {
           );
         })}
       </article>
+      <div class={tw`flex justify-end text-sm text-gray-600 mt-6`}>
+        <table class={tw`font-mono`}>
+          <tr>
+            <td>
+              <a href="../updates/2022/07/09">2022-07-10 02:10</a>
+            </td>
+            <td class={tw`pl-4`}>
+              <img
+                class={tw`inline-block align-sub mr-1`}
+                src="https://gyazo.com/9a0da4f2757af9fe1cf7bd324237a968/raw"
+                style={{ width: 18 }}
+              />
+              <a href="../authors/だいず">daiiz</a>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <a href="../updates/2022/07/09">2022-07-09 23:00</a>
+            </td>
+            <td class={tw`pl-4`}>
+              <img
+                class={tw`inline-block align-sub mr-1`}
+                src="https://gyazo.com/9a0da4f2757af9fe1cf7bd324237a968/raw"
+                style={{ width: 18 }}
+              />
+              <a href="../authors/Daiki%20Iizuka">Daiki Iizuka</a>
+            </td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
