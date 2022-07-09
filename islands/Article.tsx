e83a92b86ed347cf7a5b1267f0664d0d1548c453
daiiz <da.iz.mg@gmail.com>
2022-07-10 01:26:29 +0900

    updates, authors

--- islands/Article.tsx
+++ islands/Article.tsx
@@ -11,20 +11,27 @@ interface ArticleProps {
 
 export default function Article(props: ArticleProps) {
   return (
-    <article class={tw`w-full font-sans`}>
-      <h1 class={tw`text-2xl my-8 mt-10`}>{props.title}</h1>
-      <div class={tw`text-gray-500 mb-4`}>
-        <table class={tw`flex justify-end text-sm`}>
-          <tr>
-            <td>2022-07-09 23:00</td>
-            <td class={tw`pl-4`}>だいず</td>
-          </tr>
-        </table>
-      </div>
-      <section
-        class={tw`text-base leading-relaxed`}
-        dangerouslySetInnerHTML={{ __html: props.contentHtml }}
-      />
-    </article>
+    <div class="article">
+      <title>{props.title}</title>
+      <article class={tw`w-full font-sans`}>
+        <h1 class={tw`text-2xl my-8 mt-10`}>{props.title}</h1>
+        <div class={tw`text-gray-500 mb-4`}>
+          <table class={tw`flex justify-end text-sm`}>
+            <tr>
+              <td>
+                <a href="../updates/2022/07/09">2022-07-09 23:00</a>
+              </td>
+              <td class={tw`pl-4`}>
+                <a href="../authors/だいず">だいず</a>
+              </td>
+            </tr>
+          </table>
+        </div>
+        <section
+          class={tw`text-base leading-relaxed`}
+          dangerouslySetInnerHTML={{ __html: props.contentHtml }}
+        />
+      </article>
+    </div>
   );
 }
