d78ecf7500c8d1217f4c766b62c850e0298922bb
daiiz <da.iz.mg@gmail.com>
2022-07-13 02:28:47 +0900

    impl findRecentArticles

--- utils/db.ts
+++ utils/db.ts
@@ -1,5 +1,40 @@
+// https://deno.land/x/mongo@v0.30.1
+import { MongoClient } from "mongo";
 import "dotenv/load.ts";
 
-console.log(Deno.env.get("NODE_ENV"));
+// XXX: そのうち切り出す
+interface PimentoBlogPageSchema {
+  gcsObjectName: string;
+  title: string;
+  publishedAt: string;
+}
 
-export const findRecentArticles = async () => {};
+// 環境変数からMongoDBのURIなどを取得する
+const myMongoUri = Deno.env.get("MY_MONGODB_URI");
+const dbName = Deno.env.get("DB_NAME");
+
+if (!myMongoUri || !dbName || !myMongoUri.startsWith("mongodb+srv://")) {
+  throw new Error("MY_MONGODB_URI is not set or invalid");
+}
+
+const client = new MongoClient();
+let pimentoBlogPages = null;
+
+const connectMongo = async () => {
+  await client.connect(myMongoUri);
+  const db = client.database(dbName);
+
+  pimentoBlogPages = db.collection<PimentoBlogPageSchema>("pimentoblogpages");
+};
+
+export const findRecentArticles = async () => {
+  // if (!pimentoBlogPages) return;
+
+  const cursor = pimentoBlogPages.find({});
+  cursor.sort({ publishedAt: -1 }).limit(10);
+
+  const articles = await cursor.toArray();
+  return articles;
+};
+
+connectMongo();
