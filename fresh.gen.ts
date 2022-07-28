82af446fa39f7a49bc70b0e5e9d2d6a45f424af8
daiiz <da.iz.mg@gmail.com>
2022-07-28 11:39:25 +0900

    /docs/json 開通

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -8,14 +8,16 @@ import * as $2 from "./routes/api/joke.ts";
 import * as $3 from "./routes/api/pdf.ts";
 import * as $4 from "./routes/articles/[articleId].tsx";
 import * as $5 from "./routes/docs/[docId].tsx";
-import * as $6 from "./routes/docs/text/[docId].tsx";
-import * as $7 from "./routes/index.tsx";
+import * as $6 from "./routes/docs/json/[docId].tsx";
+import * as $7 from "./routes/docs/text/[docId].tsx";
+import * as $8 from "./routes/index.tsx";
 import * as $$0 from "./islands/Article.tsx";
 import * as $$1 from "./islands/Counter.tsx";
-import * as $$2 from "./islands/PdfDoc.tsx";
-import * as $$3 from "./islands/PdfDocCanvas.tsx";
-import * as $$4 from "./islands/RecentArticles.tsx";
-import * as $$5 from "./islands/TextDoc.tsx";
+import * as $$2 from "./islands/JsonDoc.tsx";
+import * as $$3 from "./islands/PdfDoc.tsx";
+import * as $$4 from "./islands/PdfDocCanvas.tsx";
+import * as $$5 from "./islands/RecentArticles.tsx";
+import * as $$6 from "./islands/TextDoc.tsx";
 
 const manifest = {
   routes: {
@@ -25,16 +27,18 @@ const manifest = {
     "./routes/api/pdf.ts": $3,
     "./routes/articles/[articleId].tsx": $4,
     "./routes/docs/[docId].tsx": $5,
-    "./routes/docs/text/[docId].tsx": $6,
-    "./routes/index.tsx": $7,
+    "./routes/docs/json/[docId].tsx": $6,
+    "./routes/docs/text/[docId].tsx": $7,
+    "./routes/index.tsx": $8,
   },
   islands: {
     "./islands/Article.tsx": $$0,
     "./islands/Counter.tsx": $$1,
-    "./islands/PdfDoc.tsx": $$2,
-    "./islands/PdfDocCanvas.tsx": $$3,
-    "./islands/RecentArticles.tsx": $$4,
-    "./islands/TextDoc.tsx": $$5,
+    "./islands/JsonDoc.tsx": $$2,
+    "./islands/PdfDoc.tsx": $$3,
+    "./islands/PdfDocCanvas.tsx": $$4,
+    "./islands/RecentArticles.tsx": $$5,
+    "./islands/TextDoc.tsx": $$6,
   },
   baseUrl: import.meta.url,
 };
--- routes/docs/json/[docId].tsx
+++ routes/docs/json/[docId].tsx
@@ -3,20 +3,21 @@ import "dotenv/load.ts";
 import { h } from "preact";
 import { findLatestArticle } from "@db";
 import { PageProps } from "$fresh/server.ts";
-import TextDoc from "../../../islands/TextDoc.tsx";
+import JsonDoc from "../../../islands/JsonDoc.tsx";
 
 const bucketName = Deno.env.get("GCS_BUCKET_NAME");
 
 export const handler = {
   async GET(_, ctx) {
+    // TODO: routes/docs/text/[docId].tsx と共通化する
     const docTitle = decodeURIComponent(ctx.params.docId);
     const article = await findLatestArticle(docTitle);
     const objectNameWithoutExt = article
       ? article.gcsObjectName.replace(/\.pdf$/, "")
       : "";
     let docText = "";
-    const textUrl = `https://storage.googleapis.com/${bucketName}/${objectNameWithoutExt}.txt`;
-    const res = await fetch(textUrl, { method: "GET", redirect: "follow" });
+    const jsonUrl = `https://storage.googleapis.com/${bucketName}/${objectNameWithoutExt}.json`;
+    const res = await fetch(jsonUrl, { method: "GET", redirect: "follow" });
     if (res.ok) {
       docText = await res.text();
     }
@@ -29,12 +30,12 @@ export const handler = {
   },
 };
 
-export default function DocTextPage(props: PageProps) {
+export default function DocJsonPage(props: PageProps) {
   const { docId, docTitle, docText } = props.data;
   return (
     <div data-doc-id={docId}>
       <title>{docTitle}</title>
-      <TextDoc text={docText} />
+      <JsonDoc text={docText} />
     </div>
   );
 }
