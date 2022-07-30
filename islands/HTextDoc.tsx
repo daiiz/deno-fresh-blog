fee1bb799c141b976b0e958671339fe57b371eb6
daiiz <da.iz.mg@gmail.com>
2022-07-30 17:53:06 +0900

    行ごとに表示したい

--- islands/HTextDoc.tsx
+++ islands/HTextDoc.tsx
@@ -2,23 +2,22 @@
 import { h } from "preact";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 
+const preStyle = {
+  margin: "1em 0px",
+  wordWrap: "break-word",
+  whiteSpace: "pre-wrap",
+  fontSize: "13px",
+  tabSize: 40, // 64
+};
+
 export default function HTextDoc({ text }: { text: string }) {
   if (!IS_BROWSER) {
     return <div />;
   }
+  const lines = text.split("\n");
   return (
     <div class="textdoc" style={{ padding: "0 8px" }}>
-      <pre
-        style={{
-          margin: "1em 0px",
-          wordWrap: "break-word",
-          whiteSpace: "pre-wrap",
-          fontSize: "13px",
-          tabSize: 40, // 64
-        }}
-      >
-        {text}
-      </pre>
+      <pre style={preStyle}>{lines[0]}</pre>
     </div>
   );
 }
