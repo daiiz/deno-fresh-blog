1dc8ba64719a9e594a21199d77bdef7918f908f6
daiiz <da.iz.mg@gmail.com>
2022-07-30 20:50:00 +0900

    最小限のデザイン

--- islands/HTextDoc.tsx
+++ islands/HTextDoc.tsx
@@ -2,6 +2,18 @@
 import { h } from "preact";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 
+const isGyazoBraketing = (text: string): boolean => {
+  return /^\[https?:\/\/gyazo\.com\/[0-9a-f]{32}\]$/.test(text);
+};
+
+const getGyazoThumbnailUrl = (bracketingText: string): string => {
+  const gyazoId = bracketingText.replace(
+    /^\[https?:\/\/gyazo\.com\/([0-9a-f]{32})\]$/,
+    "$1"
+  );
+  return `https://gyazo.com/${gyazoId}/max_size/1000`;
+};
+
 const LineChar = ({ char }: { char: string }) => {
   const classNames = ["char"];
   if (char === "[" || char === "]") {
@@ -15,22 +27,60 @@ const LineChar = ({ char }: { char: string }) => {
 
 const Line = ({ text, isTitle }: { text: string; isTitle: boolean }) => {
   const classNames = ["line"];
+  const contentClassNames = ["content"];
+
   if (isTitle) {
     classNames.push("title");
   }
   let renderingText = text;
   // 行頭の空白文字をタブ文字に統一する
   const [, matched] = text.match(/^(\s+)/) || [];
+  let spaceLen = 0;
   if (matched) {
-    const spaceLen = matched.length;
+    spaceLen = matched.length;
     const tabChars = "\t".repeat(spaceLen);
     renderingText = text.replace(/^\s*/, tabChars);
   }
-  const chars = [];
-  for (const [idx, char] of renderingText.split("").entries()) {
-    chars.push(<LineChar char={char} key={idx} />);
+  const chars = renderingText.split("");
+  const tabCharElems = [];
+  for (let idx = 0; idx < spaceLen; idx++) {
+    tabCharElems.push(<span>{renderingText[idx]}</span>);
   }
-  return <div class={classNames.join(" ")}>{chars}</div>;
+
+  const charElems = [];
+  if (chars[spaceLen] === ">") {
+    contentClassNames.push("quote");
+  }
+  for (let idx = spaceLen; idx < chars.length; idx++) {
+    const char = chars[idx];
+    // ブラケティングされている箇所の対応
+    if (char === "[" && chars[idx + 1] !== "[") {
+      const subStr = chars.slice(idx).join("").split("]")[0] + "]";
+      // Gyazo画像の埋め込み
+      if (isGyazoBraketing(subStr)) {
+        const srcUrl = getGyazoThumbnailUrl(subStr);
+        console.log("...", subStr, srcUrl);
+        charElems.push(<img class="image" src={srcUrl} />);
+        idx += subStr.length - 1;
+        continue;
+      }
+    }
+    charElems.push(<LineChar char={char} key={idx} />);
+  }
+
+  const contentStyle = {
+    marginLeft: `${spaceLen * 42}px`,
+  };
+  return (
+    <div>
+      <div class={classNames.join(" ")}>
+        <div class="indent">{tabCharElems}</div>
+        <div class={contentClassNames.join(" ")} style={contentStyle}>
+          {charElems}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default function HTextDoc({ text }: { text: string }) {
