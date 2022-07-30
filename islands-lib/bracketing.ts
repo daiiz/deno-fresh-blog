cc03b839eb80386aa7dc0073d2ce038406480738
daiiz <da.iz.mg@gmail.com>
2022-07-31 04:27:39 +0900

    fix

--- islands-lib/bracketing.ts
+++ islands-lib/bracketing.ts
@@ -1,10 +1,10 @@
 export const isGyazoBraketing = (text: string): boolean => {
-  return /^\[https?:\/\/gyazo\.com\/[0-9a-f]{32}\]$/.test(text);
+  return /^\[https?:\/\/gyazo\.com\/[0-9a-f]{32}(\/raw)?\]$/.test(text);
 };
 
 export const getGyazoThumbnailUrl = (bracketingText: string): string => {
   const gyazoId = bracketingText.replace(
-    /^\[https?:\/\/gyazo\.com\/([0-9a-f]{32})\]$/,
+    /^\[https?:\/\/gyazo\.com\/([0-9a-f]{32})(:?\/raw)?\]$/,
     "$1"
   );
   return `https://gyazo.com/${gyazoId}/max_size/1000`;
