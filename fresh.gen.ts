5e0dce53bdd9d5aeda4ec8da0477d282bafc0382
daiiz <da.iz.mg@gmail.com>
2022-07-31 14:36:18 +0900

    hjson 開通

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,11 +7,12 @@ import * as $1 from "./routes/api/articles.ts";
 import * as $2 from "./routes/api/joke.ts";
 import * as $3 from "./routes/api/pdf.ts";
 import * as $4 from "./routes/articles/[articleId].tsx";
-import * as $5 from "./routes/docs/htext/[docId].tsx";
-import * as $6 from "./routes/docs/json/[docId].tsx";
-import * as $7 from "./routes/docs/pdf/[docId].tsx";
-import * as $8 from "./routes/docs/text/[docId].tsx";
-import * as $9 from "./routes/index.tsx";
+import * as $5 from "./routes/docs/hjson/[docId].tsx";
+import * as $6 from "./routes/docs/htext/[docId].tsx";
+import * as $7 from "./routes/docs/json/[docId].tsx";
+import * as $8 from "./routes/docs/pdf/[docId].tsx";
+import * as $9 from "./routes/docs/text/[docId].tsx";
+import * as $10 from "./routes/index.tsx";
 import * as $$0 from "./islands/Article.tsx";
 import * as $$1 from "./islands/Counter.tsx";
 import * as $$2 from "./islands/HTextDoc.tsx";
@@ -29,11 +30,12 @@ const manifest = {
     "./routes/api/joke.ts": $2,
     "./routes/api/pdf.ts": $3,
     "./routes/articles/[articleId].tsx": $4,
-    "./routes/docs/htext/[docId].tsx": $5,
-    "./routes/docs/json/[docId].tsx": $6,
-    "./routes/docs/pdf/[docId].tsx": $7,
-    "./routes/docs/text/[docId].tsx": $8,
-    "./routes/index.tsx": $9,
+    "./routes/docs/hjson/[docId].tsx": $5,
+    "./routes/docs/htext/[docId].tsx": $6,
+    "./routes/docs/json/[docId].tsx": $7,
+    "./routes/docs/pdf/[docId].tsx": $8,
+    "./routes/docs/text/[docId].tsx": $9,
+    "./routes/index.tsx": $10,
   },
   islands: {
     "./islands/Article.tsx": $$0,
