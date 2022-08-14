9c02c6ad947f46fd54374b54ca84523ae7244117
daiiz <da.iz.mg@gmail.com>
2022-08-14 21:04:54 +0900

    skipCountを特定

--- islands-lib/deco.ts
+++ islands-lib/deco.ts
@@ -3,9 +3,20 @@ export const parseDecorationBold = (chars: string) => {
   const parsed = [];
   for (let i = 0; i < chars.length; i++) {
     const char = chars[i];
-    if (char === "[") {
-      if (chars[i + 1] === "[" && chars[i + 1] === "*") {
+    if (i === 0 && char === "[") {
+      let skipCount = 0;
+      if (chars[i + 1] === "[") {
+        // 太字記法の開始
+        skipCount = 1;
+      } else if (chars[i + 1] === "*") {
+        // 太字記法の開始
+        skipCount = chars
+          .slice(1)
+          .join("")
+          .match(/^[\*\s]+/)[0].length;
       }
+      const subStr = chars.slice(i + skipCount + 1);
+      console.log("skipCount", skipCount, subStr);
     }
   }
 
