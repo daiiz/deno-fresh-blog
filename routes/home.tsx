02ddcb9afd679f793ad4d37e7d1bb75d165c7102
daiiz <da.iz.mg@gmail.com>
2022-08-21 18:00:17 +0900

    仮

--- routes/home.tsx
+++ routes/home.tsx
@@ -1,6 +1,17 @@
 /** @jsx h */
 import { h } from "preact";
+import { tw } from "@twind";
 
 export default function Home() {
-  return null;
+  return (
+    <div>
+      <link rel="stylesheet" href="/css/global.css" />
+      <title>だいず書庫</title>
+      <div class={tw`p-4 mx-auto max-w-screen-md`}>
+        <div class={tw`flex justify-center`}>
+          <h1 class={tw`text-2xl mt-10`}>だいず書庫</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
